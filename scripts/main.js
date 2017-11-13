9230f14272d533059453d2cd321c069486cb3f35
Paul Horiachiy <pashenkagor@gmail.com>
2017-11-13 04:38:59 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 const table = host.querySelector('table');
 const leftBtn = host.querySelector('.wrapper-left');
 const topBtn = host.querySelector('.wrapper-top');
+const botBtn = host.querySelector('.tbl-btns__btn_bot');
+const rightBtn = host.querySelector('.tbl-btns__btn_right');
 leftBtn.classList.add('disabled');
 topBtn.classList.add('disabled');
 
 class TableSet {
-  static ifOneRow() {
+  ifOneRow() {
     const trTd = host.querySelectorAll('table tr');
     const td = host.querySelectorAll('td');
     if (trTd.length < 2) {
@@ -14,34 +16,34 @@ class TableSet {
     if (td.length < 2) {
       topBtn.classList.add('disabled');
     }
-  }
-  static addListener() {
+  } 
+  addListener() {
     const trTd = host.querySelectorAll('tr td');
     const wrapperTbl = host.querySelector('.tbl').getBoundingClientRect();
     const row = host.querySelectorAll('tr');
     const cel = host.querySelectorAll('td');
 
     for (let i = 0; i < row.length; i += 1) {
-      row[i].addEventListener('mouseover', () => {
-        leftBtn.rowIndex = row[i].rowIndex;
+      row[i].addEventListener('mouseenter', () => {
+      leftBtn.rowIndex = row[i].rowIndex;
       });
+    } 
+    for(let j = 0; j < cel.length; j += 1) {
+        cel[j].addEventListener('mouseenter', () => {
+          topBtn.cellIndex = cel[j].cellIndex;
+        });
     }
-    for (let i = 0; i < cel.length; i += 1) {
-      cel[i].addEventListener('mouseover', () => {
-        topBtn.cellIndex = cel[i].cellIndex;
-      });
-    }
-
     for (let i = 0; i < trTd.length; i += 1) {
+      const crds = trTd[i].getBoundingClientRect();
       trTd[i].addEventListener('mouseover', () => {
-        const crds = trTd[i].getBoundingClientRect();
-        topBtn.style = `left: ${crds.left - wrapperTbl.left}px;`;
+        //const left = crds.left - wrapperTbl.left;
+        topBtn.style = `left: ${crds.left - wrapperTbl.left}px`;
         leftBtn.style = `top: ${crds.top - wrapperTbl.top}px;`;
       });
     }
-    TableSet.tableEvents();
+    //TableSet.tableEvents();
   }
-  static addRow() {
+  addRow() {
     const tbl = host.querySelector('table tbody');
     const tableTr = host.querySelector('tr');
     let tr = null;
@@ -49,67 +51,74 @@ class TableSet {
     for (let row = 0; row < 1; row += 1) {
       tr = tableTr.cloneNode(true);
       tbl.appendChild(tr);
-      TableSet.addListener();
+      // this.addListener();
     }
   }
-  static addColumn() {
+
+
+  addColumn() {
     const tableTr = host.querySelectorAll('table tr');
     let td;
 
     for (let i = 0; i < tableTr.length; i += 1) {
       td = document.createElement('td');
       tableTr[i].appendChild(td);
-      TableSet.addListener();
+      // this.addListener();
     }
   }
-  static deleteColumn() {
+  deleteColumn() {
     const attribute = topBtn.cellIndex;
     let child = host.querySelectorAll('td');
 
     for (let i = 0; i < child.length; i +=1) {
       if ( child[i].cellIndex === attribute) {
         child[i].parentNode.removeChild(child[i]);
+        
       }
       child = host.querySelectorAll('td');
+      
     }
-    TableSet.ifOneRow();
+    
   }
-  static deleteRow() {
+  deleteRow() {
     let tableTr = host.querySelectorAll('tr');
 
     for (let i = 0; i < tableTr.length; i += 1) {
       if (tableTr[i].rowIndex == leftBtn.rowIndex) {
         tableTr[i].parentNode.removeChild(tableTr[i]);
+        
       }
       tableTr = host.querySelectorAll('tr');
+      
     }
-    TableSet.ifOneRow();
+    
   }
-  static tableEvents() {
-    const botBtn = host.querySelector('.tbl-btns__btn_bot');
-    const rightBtn = host.querySelector('.tbl-btns__btn_right');
-
+  myClassRemove() {
+    leftBtn.classList.remove('disabled');
+    topBtn.classList.remove('disabled');
+  }
+  tableEvents() {
+    let tableTr = host.querySelectorAll('tr');
+    let child = host.querySelectorAll('td');
     botBtn.addEventListener('click', this.addRow);
     rightBtn.addEventListener('click', this.addColumn);
     leftBtn.addEventListener('click', this.deleteRow); 
     topBtn.addEventListener('click', this.deleteColumn);
 
-    table.addEventListener('mouseenter', () => {
-      this.addListener();
-      leftBtn.classList.remove('disabled');
-      topBtn.classList.remove('disabled');
-      TableSet.ifOneRow();
-    });
+    table.addEventListener('mousemove', this.addListener);
+    table.addEventListener('mouseenter', this.myClassRemove);
+    table.addEventListener('mouseenter', this.ifOneRow);
     table.addEventListener('mouseleave', () => {
       leftBtn.classList.add('disabled');
       topBtn.classList.add('disabled');
+      //topBtn.style.left = '0px';
+      //leftBtn.style.top = '0px';
       topBtn.addEventListener('mouseenter', () => {
         topBtn.classList.remove('disabled');
-        TableSet.ifOneRow();
+        
       });
       leftBtn.addEventListener('mouseenter', () => {
         leftBtn.classList.remove('disabled');
-        TableSet.ifOneRow();
       });
     });
     leftBtn.addEventListener('mouseleave', () => {
@@ -120,6 +129,10 @@ class TableSet {
     });
   }
 }
-TableSet.deleteColumn();
-TableSet.deleteRow();
-TableSet.addListener();
+const a = new TableSet();
+//a.addColumn();
+//a.addRow();
+a.deleteColumn();
+a.deleteRow();
+a.tableEvents();
+a.addListener();
